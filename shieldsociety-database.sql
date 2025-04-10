475da060882aa2c178cfc4c97265a302a3c41043
khaizerdn <khaizerdn@gmail.com>
2025-04-10 22:23:12 +0800

    Update shieldsociety-database.sql

--- shieldsociety-database.sql
+++ shieldsociety-database.sql
@@ -21,11 +21,6 @@ CREATE TABLE `users` (
   `is_member` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `middle_name`, `gender`, `birthdate`, `address`, `role`, `programYearAndSection`, `student_number`, `contact_number`, `guardians_full_name`, `guardians_number`, `email`, `password`, `created_at`, `is_member`) VALUES
-(7, 'Jollibee', 'Noguera', 'Dominguez', 'Male', '2021-01-01', 'Asadasd asdasd ad asdadadadsadadadada', 'superadmin', NULL, NULL, '09356423138', NULL, NULL, 'jollibee@gmail.com', '$2b$10$3OmGs/JNvyyobwKjX.JIgexk75qQswqFiSTG6miFnN7Gtimd7viAW', '2024-12-23 23:31:20', 0),
-(9, 'Macdo', 'Noguera', 'Dominguez', 'Male', '2021-01-01', 'Asadasd asdasd ad asdadadadsadadadada', 'student', NULL, NULL, '09356423138', NULL, NULL, 'jollibee@gmail.com', '$2b$10$3OmGs/JNvyyobwKjX.JIgexk75qQswqFiSTG6miFnN7Gtimd7viAW', '2024-12-23 23:31:20', 0),
-(12, 'John', 'Doe', 'Stellar', 'Male', '2004-01-19', 'This is my address.', 'adviser', 'BSCpE 4B', '210100105', '09356423138', 'Mark Doe', '09356223166', 'cc.khaizer.noguera@cvsu.edu.ph', '$2b$10$CsRpXDQ0qB1cc1R1NvV6Mu5uDQuG.J7mhlwDsKvdDG4kW/zpNcihC', '2025-01-16 02:44:54', 0);
-
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
